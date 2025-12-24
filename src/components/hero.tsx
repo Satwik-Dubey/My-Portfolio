3e138f24f0a92d9cf863ec6b367a8becd7db2598
SatwikDubey <dubeysatwik0@gmail.com>
2025-12-25 04:38:48 +0530

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -97,7 +97,7 @@ export default function Hero() {
                 />
               </motion.div>
               <motion.div variants={fadeUp}>
-                <SocialIcon href="https://drive.google.com/file/d/1hyr631OjexG_sWaVAs5BcfhxzQ4an1dF/view?usp=sharing" icon={<File size={18} />} />
+                <SocialIcon href="https://drive.google.com/file/d/1Yi8N7OR2H0sbSsMAH9bgv_yd0FxPeojb/view?usp=sharing" icon={<File size={18} />} />
               </motion.div>
             </motion.div>
           </div>
